58b4b9753a0934f785879d7657a7c8f269d27275
Shon Vella <svella@idauto.net>
2016-09-29 14:59:57 -0600

    Added maven support

---
+++ maven-central-deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Deploy maven artifact in current directory into Maven central repository
+# using maven-release-plugin goals
+
+read -p "Really deploy to maven central repository  (yes/no)? "
+
+if [ "$REPLY" == "yes" ]; then
+  ssh-add ~/.ssh/lubos.krnac
+  ssh-add -l
+  mvn release:clean release:prepare release:perform -B -e | tee maven-central-deploy.log
+  ssh-add -D
+else
+  echo 'Exit without deploy'
+fi
