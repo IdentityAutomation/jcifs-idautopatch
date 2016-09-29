9215fa7f76c248cc8a0927b4777406579780e930
Shon Vella <svella@idauto.net>
2016-09-29 15:40:04 -0600

    Tweaking maven settings

--- maven-central-deploy.sh
+++ maven-central-deploy.sh
@@ -5,10 +5,7 @@
 read -p "Really deploy to maven central repository  (yes/no)? "
 
 if [ "$REPLY" == "yes" ]; then
-  ssh-add ~/.ssh/lubos.krnac
-  ssh-add -l
   mvn release:clean release:prepare release:perform -B -e | tee maven-central-deploy.log
-  ssh-add -D
 else
   echo 'Exit without deploy'
 fi
